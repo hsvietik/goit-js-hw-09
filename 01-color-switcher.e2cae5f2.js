1989d276c91fa86ce1477d871dbd5b814cce37ac
hsvietik <108492623+hsvietik@users.noreply.github.com>
2023-04-23 21:27:40 +0000

    Deploying to gh-pages from @ hsvietik/goit-js-hw-09@eba96e69e3c1672c55004d64450caa10bd26494f 🚀

---
+++ 01-color-switcher.e2cae5f2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o;t.addEventListener("click",(function(e){t.disabled=!0,o=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e2cae5f2.js.map
