286f08f2ce9a0857054aaeb80c67bf21d394bcf5
Hanna Svietik <hsvietik@gmail.com>
2023-04-25 12:56:09 +0100

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const amount = Number(evt.currentTarget.elements.amount.value);
+  let delay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms `);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({
+          position: position,
+          delay: delay,
+        });
+      } else {
+        rej({
+          position: position,
+          delay: delay,
+        });
+      }
+    }, delay);
+  });
 }
